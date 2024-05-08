dda7a888be1840a13e08396ff148ca7323739370
LucasJnoub <lucas.jnoub@gmail.com>
2024-05-08 16:19:53 -0300

    before uninstalling packages

--- next.config.js
+++ next.config.js
@@ -1,34 +1,7 @@
-// next.config.js
-const withPWA = require('next-pwa');
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // transpilePackages:[
-  //   "server"
-  // ],
-
-  transpilePackages: ["@piwikpro/next-piwik-pro"],
-  reactStrictMode: true,
-
-  images: {
-    domains: ['replicate.delivery'],
-  },
-};
-
-const nextConfig = withPWA({
-  dest: 'public',
-  disable: !isProduction,
-})(config);
+const config = {}
+const nextConfig = config
 
 module.exports = {
-  reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
   },
