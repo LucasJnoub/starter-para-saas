eff3d7848c42798ae8fdf685cbcd0d6209878f9e
LucasJnoub <lucas.jnoub@gmail.com>
2024-05-09 15:30:38 -0300

    removing afterauth

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,16 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook", "/pricing"],
-  afterAuth(auth, req, evt) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url});
-    }
-    // if(auth.userId && auth.isPublicRoute)  {
-    // const dashboard = new URL("/image", req.url);
-    // return NextResponse.redirect(dashboard);
-    // }
+  // afterAuth(auth, req, evt) {
+  //   if (!auth.userId && !auth.isPublicRoute) {
+  //     return redirectToSignIn({ returnBackUrl:req.url});
+  //   }
+  //   // if(auth.userId && auth.isPublicRoute)  {
+  //   // const dashboard = new URL("/image", req.url);
+  //   // return NextResponse.redirect(dashboard);
+  //   // }
 
-  }
+  // }
 });
 
 
